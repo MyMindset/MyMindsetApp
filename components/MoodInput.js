de036877e340d4cad80324de80838ecca8163cb6
cam-shotter <cameronshotter@gmail.com>
2016-11-08 23:28:40 +1300

    added bones of moodinput component

---
+++ components/MoodInput.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+export default class MoodInput extends Component {
+  constructor() {
+      super();
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.state = {
+        dataSource: ds.cloneWithRows(['row 1', 'row 2']),
+      };
+    }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData}</Text>}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#cfdfef',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('MyMindsetApp', () => MyMindsetApp);
